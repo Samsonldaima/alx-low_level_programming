40a774d77ac9a47a42abecc81d323d21870a50c3
Samsonldaima <ldaimasamson@gmail.com>
2023-03-02 05:20:02 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,9 +10,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
+void print_chessbrd(char (*a)[8]);
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-void print_number(int n);
+void set_strng(char **s, char *to);
+void print_No(int n);
 
 #endif
