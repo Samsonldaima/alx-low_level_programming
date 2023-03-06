0dcb26dd8773b5227862a1bff9bb0173d3a1d2d8
Samsonldaima <ldaimasamson@gmail.com>
2023-03-06 12:53:34 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,10 +7,13 @@
 *    represents the pointer to the
 *    block of memory to fill
 * @b: input variable of char type
-*    represents the character to fill s
-* @n: unsigned int variable the number of bytes to be filled
+*    represents the character to
+*    fill s
+* @n: unsigned int variable
+*    the number of bytes to be filled
 *
-* Return: A pointer to the filled memor area @s
+* Return: A pointer to the filled memory
+*         area @s
 */
 
 char *_memset(char *s, char b, unsigned int n)
